eb4562532859efb2cc83df2d201536727da1c0f8
Ryan Fox <rfox@kepler.space>
2021-08-28 09:21:21 -0400

    Generate JSON to instantiate each of the decks with their cards and the desired distribution of each card.

--- cards.py
+++ cards.py
@@ -1,13 +1,18 @@
 from io import BytesIO
 from collections import namedtuple
+import json
 import sys
+import time
 
 from PIL import Image, ImageDraw, ImageFont
 import cairosvg
 import openpyxl
 
-Obstacle = namedtuple('Obstacle', ['element1', 'element2', 'name', 'flavour'])
-Reward = namedtuple('Reward', ['element1', 'element2', 'name', 'text'])
+Element = namedtuple('Element', ['name', 'image', 'deck_count'])
+Obstacle = namedtuple(
+    'Obstacle', ['element1', 'element2', 'name', 'flavour', 'deck_count'])
+Reward = namedtuple('Reward',
+                    ['element1', 'element2', 'name', 'text', 'deck_count'])
 Role = namedtuple('Role', ['name', 'power'])
 
 FONT_PATH = '/usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf'
@@ -54,7 +59,7 @@ def text_wrap(text, font, max_width):
 
 def parse_spreadsheet(filename):
     wb = openpyxl.load_workbook(filename, read_only=True)
-    elements = {}
+    elements = []
     obstacles = []
     rewards = []
     roles = []
@@ -65,13 +70,13 @@ def parse_spreadsheet(filename):
         rewards_sheet = wb['Rewards']
         roles_sheet = wb['SpeciesRolesTrait']
 
-        for row in elements_sheet.iter_rows(min_row=2, max_col=2):
-            elements[row[0].value] = row[1].value
+        for row in elements_sheet.iter_rows(min_row=2, max_col=3):
+            elements.append(Element(*[cell.value for cell in row]))
 
-        for row in obstacles_sheet.iter_rows(min_row=2, max_col=4):
+        for row in obstacles_sheet.iter_rows(min_row=2, max_col=5):
             obstacles.append(Obstacle(*[cell.value for cell in row]))
 
-        for row in rewards_sheet.iter_rows(min_row=2, max_col=4):
+        for row in rewards_sheet.iter_rows(min_row=2, max_col=5):
             rewards.append(Reward(*[cell.value for cell in row]))
 
         for row in roles_sheet.iter_rows(min_row=2, min_col=6, max_col=7):
@@ -239,9 +244,6 @@ def icons_horizontal(img, xy1, xy2, icons):
 
 
 def draw_element_card(element, icon):
-    if 'macguffin' in element.lower():
-        return None
-
     img, xy1, xy2 = card_template(element, 'ELEMENT', '')
     return icons_vertical(img, xy1, xy2, [icon])
 
@@ -273,7 +275,9 @@ def draw_role_card(role):
 
 def create_image_sheets(name, images, hidden):
     if len(images) > 69:
-        create_image_sheets(name + '_', images[70:], hidden)
+        yield from create_image_sheets(name + '_', images[69:], hidden)
+
+    images = images[:69]
 
     sheet_width = 10
     if len(images) <= 10:
@@ -295,38 +299,193 @@ def create_image_sheets(name, images, hidden):
                 hidden)
     sheet.save(f'generated/{name}.png')
 
+    yield name, min(len(images), 69), sheet_width, sheet_height
+
 
 def hidden_card():
     img, xy1, xy2 = card_template('???', 'HIDDEN', '')
     return icons_vertical(img, xy1, xy2, ['hidden.svg'])
 
 
+def base_transform():
+    return {
+        'posX': 0,
+        'posY': 0,
+        'posZ': 0,
+        'rotX': 0,
+        'rotY': 180,
+        'rotZ': 180,
+        'scaleX': 1,
+        'scaleY': 1,
+        'scaleZ': 1
+    }
+
+
+def collection_json(*items):
+    return {
+        'ObjectStates': items,
+    }
+
+
+def deck_json(name, description):
+    return {
+        'Name': 'DeckCustom',
+        'Transform': base_transform(),
+        'DeckIDs': [],
+        'Nickname': name,
+        'Description': description,
+        'Hands': False,  # Don't allow decks to go to player hands
+        'SidewaysCard': False,  # Cards aren't meant to be held sideways
+        'CustomDeck': {},
+        'ContainedObjects': [],
+    }
+
+
+def subdeck_json(deck_img, backface_url, width, height):
+    base_url = 'https://raw.githubusercontent.com/rcfox/AwayTeamCards/master/generated/{}.png?{}'
+    return {
+        'FaceURL': base_url.format(deck_img, int(time.time())),
+        'BackURL': backface_url,
+        'NumWidth': width,
+        'NumHeight': height,
+        'BackIsHidden': False,  # Don't use back face as the hidden card
+        'UniqueBack': False,  # BackURL isn't a card sheet
+        'Type': 0,  # Rectangle
+    }
+
+
+def card_json(name, description, deck_count):
+    return {
+        'Name': 'Card',
+        'Transform': base_transform(),
+        'Nickname': name,
+        'Description': description,
+        'CardID': 0,
+        'Hands': True,  # Allow in player hands
+        'SidewaysCard': False,  # Not sideways
+        '__count__': deck_count,
+    }
+
+
+def define_deck(deck, deck_name, backface_url, card_data, card_imgs,
+                hidden_card):
+    for idx, sheet_data in enumerate(create_image_sheets(
+            deck_name, card_imgs, hidden_card),
+                                     start=10):
+        name, count, width, height = sheet_data
+        cards = card_data[:count]
+        card_data = card_data[count:]
+        for card_id, card in enumerate(cards):
+            deck_count = int(card.pop('__count__', 1))
+
+            card['CardID'] = int(f'{idx}{card_id:02d}')
+            for _ in range(deck_count):
+                deck['DeckIDs'].append(card['CardID'])
+                deck['ContainedObjects'].append(card)
+
+        subdeck = subdeck_json(name, backface_url, width, height)
+        deck['CustomDeck'][str(idx)] = subdeck
+
+    #with open(f'generated/{deck_name}.json', 'w') as f:
+    #    json.dump(collection_json(deck), f, indent=2)
+    return deck
+
+
+def define_element_deck(elements, hidden_card):
+    backface_url = 'https://i.imgur.com/A7Wa5GN.jpeg'
+
+    card_imgs = []
+    card_data = []
+    for element in elements:
+        if element.name is None:
+            continue
+
+        card_data.append(card_json(element.name, '', element.deck_count))
+        card_imgs.append(draw_element_card(element.name, element.image))
+
+    deck = deck_json('Elements', 'Use these to overcome obstacles.')
+
+    return define_deck(deck, 'element', backface_url, card_data, card_imgs,
+                       hidden_card)
+
+
+def define_obstacle_deck(obstacles, element_icons, hidden_card):
+    backface_url = 'https://i.imgur.com/LS2eGhT.jpg'
+
+    card_imgs = []
+    card_data = []
+    for obstacle in obstacles:
+        if obstacle.name is None:
+            continue
+
+        card_data.append(
+            card_json(obstacle.name, obstacle.flavour, obstacle.deck_count))
+        card_imgs.append(draw_obstacle_card(obstacle, element_icons))
+
+    deck = deck_json('Obstacles', 'Overcome these to win rewards.')
+
+    return define_deck(deck, 'obstacle', backface_url, card_data, card_imgs,
+                       hidden_card)
+
+
+def define_reward_deck(rewards, element_icons, hidden_card):
+    backface_url = 'https://i.imgur.com/qtH7MDU.jpg'
+
+    card_imgs = []
+    card_data = []
+    for reward in rewards:
+        if reward.text == 'Total':
+            continue
+
+        card_data.append(card_json(reward.name, reward.text,
+                                   reward.deck_count))
+        card_imgs.append(draw_reward_card(reward, element_icons))
+
+    deck = deck_json(
+        'Rewards',
+        'Bonuses to help overcome obstacles or give special abilities.')
+
+    return define_deck(deck, 'reward', backface_url, card_data, card_imgs,
+                       hidden_card)
+
+
+def define_role_deck(roles, hidden_card):
+    backface_url = 'https://hdwallpaperim.com/wp-content/uploads/2017/08/22/453064-space-space_art-stars-planet-nebula-galaxy.jpg'
+
+    card_imgs = []
+    card_data = []
+    for role in roles:
+        if role.name is None:
+            continue
+
+        card_data.append(card_json(role.name, role.power, 1))
+        card_imgs.append(draw_role_card(role))
+
+    deck = deck_json(
+        'Roles', 'Player-specific special abilities to help you in the game.')
+
+    return define_deck(deck, 'role', backface_url, card_data, card_imgs,
+                       hidden_card)
+
+
 def main():
     elements, obstacles, rewards, roles = parse_spreadsheet(sys.argv[1])
 
+    element_icons = {element.name: element.image for element in elements}
+
     hidden = hidden_card()
 
-    element_cards = []
-    for element, icon in elements.items():
-        card = draw_element_card(element, icon)
-        if card is not None:
-            element_cards.append(card)
-    create_image_sheets('element', element_cards, hidden)
+    collection = collection_json(
+        define_element_deck(elements, hidden),
+        define_obstacle_deck(obstacles, element_icons, hidden),
+        define_reward_deck(rewards, element_icons, hidden),
+        define_role_deck(roles, hidden))
 
-    obstacle_cards = []
-    for obstacle in obstacles:
-        obstacle_cards.append(draw_obstacle_card(obstacle, elements))
-    create_image_sheets('obstacle', obstacle_cards, hidden)
+    for idx, deck in enumerate(collection['ObjectStates']):
+        deck['Transform']['posX'] = idx * 2.5
 
-    reward_cards = []
-    for reward in rewards:
-        reward_cards.append(draw_reward_card(reward, elements))
-    create_image_sheets('reward', reward_cards, hidden)
-
-    role_cards = []
-    for role in roles:
-        role_cards.append(draw_role_card(role))
-    create_image_sheets('role', role_cards, hidden)
+    with open('all.json', 'w') as f:
+        json.dump(collection, f, indent=2)
 
 
 if __name__ == '__main__':
